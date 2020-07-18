edbded1b3bde9812df763af2ec7fb354f0001920
Billy Lindeman <billylindeman@gmail.com>
2020-07-18 14:23:29 -0400

    Stubs for subscription/session management for signal.rs

--- switchboard-rs/src/signal.rs
+++ switchboard-rs/src/signal.rs
@@ -5,7 +5,7 @@ use serde::{Serialize,Deserialize};
 use std::collections::{HashMap};
 use std::sync::{atomic, Arc, RwLock};
 
-use jsonrpc_core::{Result, Error};
+use jsonrpc_core::{Result, Error, ErrorCode};
 use jsonrpc_derive::rpc;
 use jsonrpc_ws_server::{ServerBuilder,RequestContext};
 
@@ -45,15 +45,14 @@ pub struct SubscribeReply{
 #[rpc]
 pub trait SignalService {
     type Metadata;
-
     
 	/// Join room 
 	#[pubsub(subscription = "room", subscribe, name = "room_join")]
-	fn join(&self, meta: Self::Metadata, subscriber: typed::Subscriber<String>, param: u64);
+	fn room_join(&self, meta: Self::Metadata, subscriber: typed::Subscriber<RoomEvent>, param: u64);
 
 	/// Leave room
 	#[pubsub(subscription = "room", unsubscribe, name = "room_leave")]
-	fn leave(&self, meta: Option<Self::Metadata>, subscription: SubscriptionId) -> Result<bool>;
+	fn room_leave(&self, meta: Option<Self::Metadata>, subscription: SubscriptionId) -> Result<bool>;
 
     #[rpc(name = "stream_list")]
     fn stream_list(&self) -> Result<Vec<String>>;
@@ -63,7 +62,6 @@ pub trait SignalService {
 
     #[rpc(name = "stream_subscribe")]
 	fn stream_subscribe(&self, offer: String) -> Result<SubscribeRequest>;
-
 
 }
 
@@ -77,11 +75,46 @@ pub struct Server {
 impl SignalService for Server {
 	type Metadata = Arc<Session>;
 
-	fn PublishStream(&self, req: PublishRequest) -> Result<PublishReply> {
+	fn room_join(&self, _meta: Self::Metadata, subscriber: typed::Subscriber<RoomEvent>, param: u64) {
+		if param != 10 {
+			subscriber
+				.reject(Error {
+					code: ErrorCode::InvalidParams,
+					message: "Rejecting subscription - invalid parameters provided.".into(),
+					data: None,
+				})
+				.unwrap();
+			return;
+		}
+
+		let id = self.uid.fetch_add(1, atomic::Ordering::SeqCst);
+		let sub_id = SubscriptionId::Number(id as u64);
+		let sink = subscriber.assign_id(sub_id.clone()).unwrap();
+		self.active.write().unwrap().insert(sub_id, sink);
+	}
+
+	fn room_leave(&self, _meta: Option<Self::Metadata>, id: SubscriptionId) -> Result<bool> {
+		let removed = self.active.write().unwrap().remove(&id);
+		if removed.is_some() {
+			Ok(true)
+		} else {
+			Err(Error {
+				code: ErrorCode::InvalidParams,
+				message: "Invalid subscription.".into(),
+				data: None,
+			})
+		}
+	}
+
+    fn stream_list(&self) -> Result<Vec<String>> {
+        Ok([].into())
+    }
+
+	fn stream_publish(&self, offer: String) -> Result<PublishReply> {
         Err(Error::internal_error())
     }
 
-	fn SubscribeStream(&self, req: SubscribeRequest) -> Result<SubscribeRequest> {
+	fn stream_subscribe(&self, offer: String) -> Result<SubscribeRequest> {
         Err(Error::internal_error())
     }
 
