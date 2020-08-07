4e34c921b0182acd99b49758211d565834faea78
Billy Lindeman <billylindeman@gmail.com>
2020-08-06 23:10:53 -0400

    Basic template for yew project stubbed out

---
+++ switchboard-web/src/app.rs
@@ -0,0 +1,42 @@
+use wasm_bindgen::prelude::*;
+use yew::prelude::*;
+
+pub struct App {
+    link: ComponentLink<Self>,
+    value: i64,
+}
+
+pub enum Msg {
+    AddOne,
+}
+
+impl Component for App {
+    type Message = Msg;
+    type Properties = ();
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self { link, value: 0 }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::AddOne => self.value += 1,
+        }
+        true
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        // Should only return "true" if new properties are different to
+        // previously received properties.
+        // This component has no properties so we will always return "false".
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <div>
+                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
+                <p>{ self.value }</p>
+            </div>
+        }
+    }
+}
